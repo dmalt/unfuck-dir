4f0890f096b42aeeac2d059274fc19eb6e7ed25b
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-06-23 12:05:52 +0200

    reimplement get_state_dir as one platform-agnostic fn

--- src/history.rs
+++ src/history.rs
@@ -1,5 +1,33 @@
-fn hello() {
-    println!("hello, world")
+use std::env;
+use std::path::PathBuf;
+
+const STATE_DIRNAME: &str = "unfk";
+
+pub fn get_state_dir(os: &str) -> Option<PathBuf> {
+    match os {
+        "macos" => env::var("HOME")
+            .map(|x| {
+                PathBuf::from(x)
+                    .join("Library")
+                    .join("Application Support")
+                    .join(STATE_DIRNAME)
+            })
+            .ok(),
+        "linux" | "freebsd" | "openbsd" | "netbsd" | "dragonfly" => env::var("XDG_STATE_HOME")
+            .ok()
+            .map(PathBuf::from)
+            .filter(|p| p.is_absolute())
+            .or_else(|| {
+                env::var("HOME")
+                    .ok()
+                    .map(|home| PathBuf::from(home).join(".local").join("state"))
+            })
+            .map(|x| x.join(STATE_DIRNAME)),
+        "windows" => env::var("LOCALAPPDATA")
+            .ok()
+            .map(|p| PathBuf::from(p).join(STATE_DIRNAME)),
+        _ => None,
+    }
 }
 
 #[cfg(test)]
